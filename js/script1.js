b49427a3e78ec3dfdbd60ae42467f3c72b1a5148
Joseph Hilber <jahilber@Josephs-MacBook-Air.local>
2020-04-28 16:01:00 -0400

    Minor style adjustments.

--- js/script1.js
+++ js/script1.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
     
     
-    var resizeBeforeFooter = function() {
+    var resizeContactCards = function() {
         var h = .05 * $(window).height();
         if (h < 60) {
             h = 60;
@@ -13,9 +13,29 @@ $(document).ready(function() {
         $("#contactCards").css("margin-bottom", h.toString() + "px").css("margin-top", h.toString() + "px");
     };
     
+    var resizeCardsText = function() {
+        var w = $(window).width();
+        if (w < 500) {
+            $(".cardsTitle").css("font-size", "14px");
+        } else if (w <= 1000) {
+            $(".cardsTitle").css("font-size", "14px");
+        } else if (w <= 1800){
+            $(".cardsTitle").css("font-size", "14px");
+        } else {
+            $(".cardsTitle").css("font-size", parseInt((w / 64.2), 10).toString() + "px");
+        }
+    };
+    
+    var resizeCardsInternal = function() {
+        var h = $(".cardsGUI").height();
+        $(".cardsTitle").css("margin-top", parseInt(.2 * h).toString() + "px");
+    }
+    
     
     $(window).bind('resize', function() {
-        resizeBeforeFooter();
+        resizeContactCards();
+        resizeCardsText();
+        resizeCardsInternal();
     }).trigger('resize');
     
 });
